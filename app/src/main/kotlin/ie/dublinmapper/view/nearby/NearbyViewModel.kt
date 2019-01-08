09afc7647c8181a24af561b5cb4eaf6508a4ac01
Conor O'Brien <conor.w.ob@gmail.com>
2019-01-08 00:48:43 +0000

    Fix for map icon flickering due to combineLatest()

--- app/src/main/kotlin/ie/dublinmapper/view/nearby/NearbyViewModel.kt
+++ app/src/main/kotlin/ie/dublinmapper/view/nearby/NearbyViewModel.kt
@@ -7,6 +7,7 @@ import ie.dublinmapper.domain.usecase.NearbyUseCase
 import ie.dublinmapper.util.Coordinate
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class NearbyViewModel @Inject constructor(
@@ -17,6 +18,7 @@ class NearbyViewModel @Inject constructor(
 
     fun onCameraMoved(coordinate: Coordinate) {
         nearbyUseCase.getNearbyServiceLocations(coordinate)
+            .debounce(100L, TimeUnit.MILLISECONDS)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnNext { nearbyServiceLocations.value = it }
