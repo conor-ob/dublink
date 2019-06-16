7286f177209e20e752d35d9d2617c0695875ea96
Conor O'Brien <conor.w.ob@gmail.com>
2019-06-16 17:18:26 +0100

    Refactor luas and aircoach LiveData list items and extract styling to styles

--- app/src/main/kotlin/ie/dublinmapper/model/luas/LuasLiveDataItem.kt
+++ app/src/main/kotlin/ie/dublinmapper/model/luas/LuasLiveDataItem.kt
@@ -1,31 +1,32 @@
 package ie.dublinmapper.model.luas
 
-import com.xwray.groupie.kotlinandroidextensions.Item
+import android.content.res.ColorStateList
+import androidx.core.content.ContextCompat
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import ie.dublinmapper.R
-import ie.dublinmapper.domain.model.LuasLiveData
 import ie.dublinmapper.model.LuasLiveDataUi
-import ie.dublinmapper.util.StringUtils
-import kotlinx.android.synthetic.main.list_item_live_data_luas.*
-import java.util.*
+import ie.dublinmapper.model.livedata.LiveDataItem
+import kotlinx.android.synthetic.main.list_item_live_data.*
 
 class LuasLiveDataItem(
-    private val liveData: LuasLiveDataUi
-) : Item() {
+    private val liveData: LuasLiveDataUi,
+    isEven: Boolean,
+    isLast: Boolean
+) : LiveDataItem(isEven, isLast) {
 
-    override fun getLayout() = R.layout.list_item_live_data_luas
+    override fun getLayout() = R.layout.list_item_live_data
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        viewHolder.operator.text = liveData.liveData.operator.name
-        viewHolder.direction_destination.text = StringUtils.join(
-            Arrays.asList(
-                liveData.liveData.route,
-                liveData.liveData.destination
-            ), " ${StringUtils.MIDDLE_DOT} ")
-        if (liveData.liveData.dueTime[0].minutes == 0L) {
-            viewHolder.due.text = viewHolder.itemView.resources.getString(R.string.live_data_due)
-        } else {
-            viewHolder.due.text = viewHolder.itemView.resources.getString(R.string.live_data_due_time, liveData.liveData.dueTime[0].minutes)
+        super.bind(viewHolder, position)
+        super.bindDueTimes(viewHolder, liveData.liveData.dueTime)
+        val liveData = liveData.liveData
+        viewHolder.title.text = liveData.destination
+        viewHolder.subtitle.text = liveData.route
+        viewHolder.operatorIconContainer.setImageResource(R.drawable.ic_tram)
+        when {
+            liveData.route.equals("Red", ignoreCase = true) -> viewHolder.operatorIconContainer.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(viewHolder.itemView.context, R.color.luasRed))
+            liveData.route.equals("Green", ignoreCase = true) -> viewHolder.operatorIconContainer.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(viewHolder.itemView.context, R.color.luasGreen))
+            else -> viewHolder.operatorIconContainer.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(viewHolder.itemView.context, R.color.luasPurple))
         }
     }
 
