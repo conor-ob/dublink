f97f0399183d27c23ce6ef76790c2edf8fdce712
Conor O'Brien <conor.w.ob@gmail.com>
2019-10-21 23:57:33 +0100

    Add HTTP logging interceptor

--- repository/src/main/kotlin/ie/dublinmapper/repository/di/RepositoryModule.kt
+++ repository/src/main/kotlin/ie/dublinmapper/repository/di/RepositoryModule.kt
@@ -11,6 +11,7 @@ import ie.dublinmapper.repository.favourite.FavouriteRepositoryModule
 import ie.dublinmapper.repository.irishrail.IrishRailRepositoryModule
 import ie.dublinmapper.repository.luas.LuasRepositoryModule
 import io.rtpi.client.RtpiClient
+import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
@@ -30,7 +31,18 @@ class RepositoryModule {
 
     @Provides
     @Singleton
-    fun client(): RtpiClient = RtpiClient()
+    fun okHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addNetworkInterceptor(NetworkLoggingInterceptor())
+            .retryOnConnectionFailure(true)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+
+    @Provides
+    @Singleton
+    fun client(okHttpClient: OkHttpClient): RtpiClient = RtpiClient(okHttpClient)
 
     @Provides
     @Named("SHORT_TERM")
