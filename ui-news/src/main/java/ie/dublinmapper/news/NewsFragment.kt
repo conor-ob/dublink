b49ae5fa8f329182d7d16e107145bd9fafb0fd1e
Conor O'Brien <conor.w.ob@gmail.com>
2019-11-30 01:32:24 +0000

    Twitter feeds for all transport services

--- ui-news/src/main/java/ie/dublinmapper/news/NewsFragment.kt
+++ ui-news/src/main/java/ie/dublinmapper/news/NewsFragment.kt
@@ -1,16 +1,13 @@
 package ie.dublinmapper.news
 
 import android.content.pm.PackageManager
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.twitter.sdk.android.core.Twitter
 import com.twitter.sdk.android.core.TwitterAuthConfig
 import com.twitter.sdk.android.core.TwitterConfig
-import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter
-import com.twitter.sdk.android.tweetui.TwitterListTimeline
 import ie.dublinmapper.ui.DublinMapperFragment
+import io.rtpi.api.Service
 import kotlinx.android.synthetic.main.fragment_news.*
 
 class NewsFragment : DublinMapperFragment(R.layout.fragment_news) {
@@ -41,27 +38,34 @@ class NewsFragment : DublinMapperFragment(R.layout.fragment_news) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val timeline = TwitterListTimeline.Builder()
-            .slugWithOwnerScreenName("dublin-bus", "dublinbuspal")
-            .includeRetweets(java.lang.Boolean.TRUE)
-            .build()
-        val adapter = TweetTimelineRecyclerViewAdapter.Builder(context)
-            .setTimeline(timeline)
-            .setViewStyle(getViewStyle())
-            .build()
-
-        twitterFeed.adapter = adapter
-        twitterFeed.setHasFixedSize(true)
-        twitterFeed.layoutManager = LinearLayoutManager(requireContext())
+        viewpager.adapter = TwitterFeedAdapter(
+            getSortedServices(),
+            childFragmentManager
+        )
+        tabs.setupWithViewPager(viewpager)
     }
 
-    private fun getViewStyle(): Int {
-        return when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_NO -> R.style.tw__TweetLightStyle
-            Configuration.UI_MODE_NIGHT_YES -> R.style.TwitterFeed
-            else -> R.style.tw__TweetLightStyle
+    private fun getSortedServices(): List<Service> {
+        val services = mutableListOf<Service>()
+        if (enabledServiceManager.isServiceEnabled(Service.DUBLIN_BUS)) {
+            services.add(Service.DUBLIN_BUS)
         }
+        if (enabledServiceManager.isServiceEnabled(Service.IRISH_RAIL)) {
+            services.add(Service.IRISH_RAIL)
+        }
+        if (enabledServiceManager.isServiceEnabled(Service.LUAS)) {
+            services.add(Service.LUAS)
+        }
+        if (enabledServiceManager.isServiceEnabled(Service.BUS_EIREANN)) {
+            services.add(Service.BUS_EIREANN)
+        }
+        if (enabledServiceManager.isServiceEnabled(Service.DUBLIN_BIKES)) {
+            services.add(Service.DUBLIN_BIKES)
+        }
+        if (enabledServiceManager.isServiceEnabled(Service.AIRCOACH)) {
+            services.add(Service.AIRCOACH)
+        }
+        return services
     }
 
 }
---
+++ ui-news/src/main/java/ie/dublinmapper/news/TwitterFragment.kt
@@ -0,0 +1,39 @@
+package ie.dublinmapper.news
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter
+import com.twitter.sdk.android.tweetui.TwitterListTimeline
+import ie.dublinmapper.ui.DublinMapperFragment
+import kotlinx.android.synthetic.main.fragment_twitter.*
+
+class TwitterFragment(private val listName: String) : DublinMapperFragment(R.layout.fragment_twitter) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val timeline = TwitterListTimeline.Builder()
+            .slugWithOwnerScreenName(listName, "dublinbuspal")
+            .includeRetweets(java.lang.Boolean.TRUE)
+            .build()
+        val adapter = TweetTimelineRecyclerViewAdapter.Builder(context)
+            .setTimeline(timeline)
+            .setViewStyle(getViewStyle())
+            .build()
+
+        twitterFeed.adapter = adapter
+        twitterFeed.setHasFixedSize(true)
+        twitterFeed.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun getViewStyle(): Int {
+        return when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> R.style.tw__TweetLightStyle
+            Configuration.UI_MODE_NIGHT_YES -> R.style.TwitterFeed
+            else -> R.style.tw__TweetLightStyle
+        }
+    }
+
+}
