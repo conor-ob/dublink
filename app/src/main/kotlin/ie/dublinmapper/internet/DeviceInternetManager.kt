620a8b173355ad787f632516022ac3a78eb2ce55
Conor O'Brien <conor.w.ob@gmail.com>
2020-03-29 04:50:22 +0100

    Ping google servers to check for internet

--- app/src/main/kotlin/ie/dublinmapper/internet/DeviceInternetManager.kt
+++ app/src/main/kotlin/ie/dublinmapper/internet/DeviceInternetManager.kt
@@ -6,30 +6,70 @@ import android.net.wifi.SupplicantState
 import ie.dublinmapper.domain.service.InternetManager
 import timber.log.Timber
 import java.io.IOException
+import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 import android.net.wifi.WifiManager as AndroidWifiManager
 
 class DeviceInternetManager(private val context: Context) : InternetManager {
 
-    override fun isConnected(): Boolean {
-        if (isNetworkAvailable()) {
-            try {
-                val connection = URL("http://clients3.google.com/generate_204")
-                    .openConnection() as HttpURLConnection
-                connection.setRequestProperty("User-Agent", "Android")
-                connection.setRequestProperty("Connection", "close")
-                connection.connectTimeout = 10000
-                connection.connect()
-                val result = connection.responseCode == 204 && connection.contentLength == 0
-                connection.disconnect()
-                return result
-            } catch (e: IOException) {
-                Timber.e(e)
+    private val googleServers = listOf(
+        "http://connectivitycheck.gstatic.com/generate_204",
+        "http://clients3.google.com/generate_204",
+        "http://clients1.google.com/generate_204",
+        "http://clients2.google.com/generate_204",
+        "http://clients4.google.com/generate_204",
+        "http://clients5.google.com/generate_204",
+        "http://clients6.google.com/generate_204",
+        "http://www.google.com/gen_204",
+        "http://www.gstatic.com/generate_204",
+        "http://maps.google.com/generate_204",
+        "http://mt0.google.com/generate_204",
+        "http://mt1.google.com/generate_204",
+        "http://mt2.google.com/generate_204",
+        "http://mt3.google.com/generate_204",
+        "https://www.gstatic.com/generate_204",
+        "https://gstatic.com/generate_204"
+    )
+
+    override fun isConnected() = if (isNetworkAvailable()) {
+        googleServers
+            .asSequence()
+            .map { tryPing(it) }
+            .first { true }
+    } else {
+        false
+    }
+
+    private fun tryPing(server: String): Boolean {
+        var urlConnection: HttpURLConnection? = null
+        try {
+            Timber.d("Attempting to ping $server")
+            urlConnection = URL(server).openConnection() as HttpURLConnection
+//            urlConnection.instanceFollowRedirects = false
+//            urlConnection.useCaches = false
+//            urlConnection.allowUserInteraction = false
+//            urlConnection.requestMethod = "GET"
+            urlConnection.setRequestProperty("User-Agent", "Android")
+            urlConnection.setRequestProperty("Connection", "close")
+            urlConnection.connectTimeout = 10000
+            urlConnection.readTimeout = 10000
+            urlConnection.connect()
+            val successful = urlConnection.responseCode == 204 && urlConnection.contentLength == 0
+            if (successful) {
+                Timber.d("Pinged $server")
+                return successful
             }
-        } else {
-            Timber.d("No network available")
+        } catch (e : IOException) {
+            Timber.e(e)
+            Timber.d("Error while pinging $server")
+        } catch (e: Exception) {
+            Timber.e(e)
+            Timber.d("Error while pinging $server")
+        } finally {
+            urlConnection?.disconnect()
         }
+        Timber.d("Failed to ping $server")
         return false
     }
 
