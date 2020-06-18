9731015f97c31277bd3a14ebc54d38310e8068f2
Conor O'Brien <conor.w.ob@gmail.com>
2020-06-18 22:35:07 +0100

    Release 1.1.0 (#88)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,7 +5,7 @@ object BuildConfig {
 
     object Version {
         private const val major = 1
-        private const val minor = 0
+        private const val minor = 1
         private const val patch = 0
 
         const val name = "$major.$minor.$patch"
